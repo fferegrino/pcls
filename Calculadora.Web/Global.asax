8f4ba12879e85525feef3d6abe531576e2347947
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-10-31 09:22:09 -0600

    Projects for sample
    Needs clean up

---
+++ Calculadora.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Calculadora.Web.Global" Language="C#" %>
---
+++ PclCalculadora/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PclCalculadora
+{
+    public class Class1
+    {
+    }
+}
